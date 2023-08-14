2f9c2de50b282b726a84f417e54d18854cb4a6cc
Joe Lohr <jlohr@autorisknow.com>
2023-08-14 12:43:09 -0500

    Switched logo added wire:navigate

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -12,8 +12,8 @@
 <body>
     <header>
         <div class="w-full max-w-5xl mx-auto flex justify-between my-5 px-5 lg:px-0">
-            <a href="/" class="hover:opacity-75 transition">
-                <svg class="w-full h-10" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 165.83 48.75"><defs><style>.cls-5{letter-spacing:-.02em}</style></defs><text fill="#231f20" font-family="OfeliaDisplay-Medium,&quot;Ofelia Display&quot;" font-size="44" font-weight="500" transform="translate(0 38.5)"><tspan x="0" y="0" letter-spacing="-.05em">L</tspan><tspan x="19.27" y="0" class="cls-5">oh</tspan><tspan x="71.1" y="0" letter-spacing="-.02em">r</tspan><tspan x="87.96" y="0" letter-spacing="-.04em">a</tspan><tspan x="110.53" y="0" letter-spacing="-.05em">v</tspan><tspan x="131.03" y="0" class="cls-5">el</tspan></text></svg>
+            <a wire:navigate href="/" class="hover:opacity-75 transition">
+                <img class="w-full h-10" src="{{ asset('images/logo.svg') }}" alt="Lohravel">
             </a>
             <div></div>
         </div>
